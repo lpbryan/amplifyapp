59abb4fab523f1f4e65f541ce484af0c2660e198
lpbryan <lpbryan@mail.fhsu.edu>
2022-09-02 12:27:50 -0500

    changes to api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 import { API } from 'aws-amplify';
 import { 
   withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
  } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
